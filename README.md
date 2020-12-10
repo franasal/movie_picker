8df716b010e2ed70dbc950e4ac3b40f162086af1
franasal <farcilas@gmail.com>
2020-12-10 13:10:48 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # :movie_camera: movie_picker :movie_camera:
+
+[try it out](https://share.streamlit.io/franasal/movie_picker/movie_picker.py)
--- movie_picker.py
+++ movie_picker.py
@@ -4,31 +4,30 @@ from time import sleep
 import streamlit as st
 
 st.title(':movie_camera: AWESOME MOVIE PICKER :movie_camera:')
-st.header('For CAFE')
+st.header("For CAFE' s WG")
 
 # @st.cache
 def get_movie_data():
 
-    df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRnJSiajjr_c9JvB-AwFGXJftAKUcZSKSylD4yhJRojK30bZO5BNxeIVafRACILnKAZIRsMlKbGKJZK/pub?output=csv", sep=',')
+    df = pd.read_excel("https://docs.google.com/spreadsheets/d/e/2PACX-1vRnJSiajjr_c9JvB-AwFGXJftAKUcZSKSylD4yhJRojK30bZO5BNxeIVafRACILnKAZIRsMlKbGKJZK/pub?output=xlsx",
+                                    sheet_name=0)
     return df
-
-
 
 def main():
     df = get_movie_data()
 
     st.header("Pick some genres:")
-    genres = st.multiselect(
-        "(or leave it empty to use the whole list)", list(df.GENRE.unique()), []
+    st.info("Please make a genre selection (or leave it empty to use the whole list).")
+    genres = st.multiselect("",list(df.GENRE.unique()), []
     )
-    if not genres:
-        st.warning("Please select at least one genre.")
 
     if st.button('Pick a movie!'):
         choose_film(df, genres)
             # clf, confusion_matrix = train_rf(df, cols_to_train)
         st.balloons()
             # st.pyplot(confusion_matrix)
+
+    st.write("\n\n:point_up:Dont't forget to mark this movie as watched on the [Movie List](https://docs.google.com/spreadsheets/d/1kpLW_3n1OtmTDNneaGUMG5ch94RD_wt5QzNoLChPRt4/edit#gid=0)")
 
 
 
@@ -44,15 +43,19 @@ def choose_film(movie_df, genres_list):
     a movie picker for the wg 77a2R!!!
     """
 
+    movie_df['WATCHED'] = movie_df['WATCHED'].astype('bool')
     filtered_movies=(movie_df[movie_df[
         'GENRE'].isin(genres_list) & movie_df['WATCHED']==False])
     random_num=randint(1, len(filtered_movies)-1)
-    st.text(f"performing super complex calculations\nto pick an awesome {genres_list} movie ")
+    if genres_list:
+        st.text(f"performing super complex calculations\nto pick an awesome {genres_list} movie ")
+    else:
+        st.text(f"performing super complex calculations\nto pick an awesome movie ")
     st.header(" \n\n Tonight you will watch: ")
     print_prog_bar()
 
 
-    st.title(f":fire:{filtered_movies['MOVIE_TITLE'][random_num]}:fire::fire:")
+    st.title(f":fire: {filtered_movies['MOVIE_TITLE'][random_num]} :fire:")
 
 # selected_genres
 
